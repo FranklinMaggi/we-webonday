650cbbf6ae57aecf2006d044df200d5cf207361b
Francesco Maggi <franklin19@outlook.it>
2026-01-07 12:02:01 +0100

    config

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,6 +1,8 @@
 // src/lib/config.ts
 
 
-export const API_BASE =
-  import.meta.env.VITE_API_BASE;
+export const API_BASE = import.meta.env.VITE_API_URL;
 
+if (!API_BASE) {
+  throw new Error("FATAL: VITE_API_URL non definita");
+}
