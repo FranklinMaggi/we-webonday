e6662319df39daa5db12a9fe7daea974f8e6a0f6
Francesco Maggi <franklin19@outlook.it>
2026-01-07 16:23:40 +0100

    establisced env

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -11,7 +11,7 @@
 //
 // ======================================================
 
-export const API_BASE = import.meta.env.VITE_API_BASE ?? "";
+export const API_BASE :string = import.meta.env.VITE_API_BASE ;
 
 if (!API_BASE) {
   console.error(
