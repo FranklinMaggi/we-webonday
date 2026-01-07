606ed67c96957a01d51ce9009f46d624147c8565
Francesco Maggi <franklin19@outlook.it>
2026-01-07 01:43:22 +0100

    solution in right place

--- frontend/src/pages/user/dashboard/sections/ExploreSolutionsCTA.tsx
+++ frontend/src/pages/user/dashboard/sections/ExploreSolutionsCTA.tsx
@@ -16,7 +16,7 @@ import { Link } from "react-router-dom";
 export default function ExploreSolutionsCTA() {
   return (
     <section>
-      <Link to="/" className="user-cta primary">
+      <Link to="/solution" className="user-cta primary">
         Esplora le soluzioni disponibili
       </Link>
     </section>
